ca2b1cd13fd7158e7ea08bc3d0bdfef099207b45
cuhong <hongcoilhouse@gmail.com>
2019-07-04 22:53:37 +0900

    first commit

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 import shutil
 
 from django.contrib.auth.models import Permission
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -11,6 +12,9 @@ from users.models import CustomUser, ResearcherUser
 def researcheruser_post_save(sender, instance, created, raw, using, **kwargs):
     if created:
         if instance.is_researcher:
-            permissions = Permission.objects.filter(codename__icontains='researchadminproxyforresearch')
+            models = ['researchadminproxyforresearch', 'game', 'agree']
+            for model in models:
+                q |= Q(codename__icontains=model)
+            permissions = Permission.objects.filter(q)
             instance.user_permissions.set(permissions)
             instance.save()
