708007b870f59aae7be74ed4158e5221bd080132
cuhong <hongcoilhouse@gmail.com>
2019-07-04 23:09:41 +0900

    first commit

--- participate/views.py
+++ participate/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views import View
 from django.views.generic import TemplateView
 
+from research.models import Research
+
 
 class ParticipateView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'agree.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ParticipateView, self).get_context_data(**kwargs)
+        research_hex = self.kwargs['research_hex']
+        context['research'] = get_object_or_404(Research, hex=research_hex)
+        return context
