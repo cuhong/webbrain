6f5d40a230ce8709f9129c6f3ff97cd6836d5ed1
cuhong <hongcoilhouse@gmail.com>
2019-08-02 10:06:04 +0900

    게임 종료 후 점수 표시

--- participate/views.py
+++ participate/views.py
@@ -25,11 +25,11 @@ from django.contrib.auth import views as auth_views
 
 class IndexView(TemplateView):
     template_name = 'frontend/index.html'
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['page'] = MainPage.objects.get()
-    #     return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = MainPage.objects.get()
+        return context
 
 
 class ParticipateListView(LoginRequiredMixin, TemplateView):
