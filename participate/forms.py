100ea20535221e2c2f63e878134228aa84ee3bc8
cuhong <hongcoilhouse@gmail.com>
2019-08-11 12:50:59 +0900

    게임 종료 후 점수 표시

--- participate/forms.py
+++ participate/forms.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 from participate.models import Participate
 
 
-class ResearchAgreeForm(forms.Form):
-    # class Meta:
-    #     model = Participate
-    #     fields = ['agree_name', 'agree_date']
+class ResearchAgreeForm(forms.ModelForm):
+    class Meta:
+        model = Participate
+        fields = ['agree_name', 'agree_date']
 
     agree_date = forms.DateField(widget=DateTimePicker(options={'format': "YYYY-MM-DD", "pickSeconds": False}), initial=timezone.now().date(), label='동의일자')
