f8ff1b7e169413ea941613708e741ebe8ab567ca
cuhong <hongcoilhouse@gmail.com>
2019-08-11 19:29:43 +0900

    다시하기 기능

--- webbrain/settings.py
+++ webbrain/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middleware.PreventConcurrentLoginsMiddleware',
+    # 'users.middleware.PreventConcurrentLoginsMiddleware',
 ]
 
 ROOT_URLCONF = 'webbrain.urls'
