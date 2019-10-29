05a874b0733fc4df643b5ad619e85715f71cbaea
cuhong <hongcoilhouse@gmail.com>
2019-10-29 20:04:35 +0900

    연세대 서버 추가

--- webbrain/settings.py
+++ webbrain/settings.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'e$mu))=4o0(x4=n5auxg=^+w^oy7z2*@l%i8^g&wh$6on@prqw'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '13.124.126.141', 'brain.yonsei.ac.kr']
+ALLOWED_HOSTS = ['127.0.0.1', '13.124.126.141', 'brain.yonsei.ac.kr', '13.124.82.23']
 
 INSTALLED_APPS = [
     'jet',
